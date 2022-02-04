d187155eaad2bcfe58aef36eec15362ce7829fbf
Florian <f.aumeier@me.com>
2022-02-04 14:56:43 +0000

    add Perspective decoration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <>
       <CharacterEditor />
+      <div className={styles.greyBar} />
       <Footer />
     </>
   );
